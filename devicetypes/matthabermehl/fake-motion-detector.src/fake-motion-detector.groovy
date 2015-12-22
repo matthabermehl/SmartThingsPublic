40c1f979821e1a2f3e4daa6039bb0847ff63c235
Matt Habermehl <matt.habermehl@gmail.com>
2015-12-22 00:32:53 -0500

    initial commit

---
+++ devicetypes/matthabermehl/fake-motion-detector.src/fake-motion-detector.groovy
@@ -0,0 +1,51 @@
+/**
+ *  Fake Motion Detector
+ *
+ *  Copyright 2015 Matt Habermehl
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ * 	A simple device type that is meant to take commands to broadcast motion or motion stopping.
+ *	Obvious use is to trigger SmartApps that use motion detection from another app.
+ */
+metadata {
+	definition (name: "Fake Motion Detector", namespace: "matthabermehl", author: "Matt Habermehl") {
+		capability "Motion Sensor"
+
+		command "activate"
+		command "deactivate"
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+}
+
+// handle commands
+def activate() {
+	log.debug "Executing 'activate'"
+    sendEvent(
+		name: motion,
+		value: active,
+		descriptionText: "Motion simulated"
+	)
+}
+
+def deactivate() {
+	log.debug "Executing 'deactivate'"
+	sendEvent(
+		name: motion,
+		value: inactive,
+		descriptionText: "Motion stop simulated"
+	)
+}
+
+
